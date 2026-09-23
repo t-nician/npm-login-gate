4194ea2ef2e0c35737629a16189250dcdeaad6e8
t-nician <browne.kevinjr@gmail.com>
2026-09-23 05:29:49 -0400

    Looking clean!

--- src/gate/logic.py
+++ src/gate/logic.py
@@ -1,11 +1,18 @@
 from gate import model, env
 
 
+cached_attempts: dict[str, int] = {}
 
 
 async def is_timed_out(address: str):
-    pass
+    return await model.Timedout.get_or_none(address=address) != None
 
 
 async def is_whitelisted(address: str):
-    pass
+    return await model.Whitelisted.get_or_none(address=address) != None
+
+
+async def attempt_login(address: str, password: str):
+    
+    
+    return "You have been whitelisted!"
--- src/gate/server.py
+++ src/gate/server.py
@@ -14,11 +14,36 @@ class LoginBody(BaseModel):
 
 
 async def home_endpoint(request: Request):
-    pass
+    nginx_address = request.client.host
+    client_address = request.headers.get("x-real-ip")
+    
+    if nginx_address != env.NGINX_HOST:
+        return "Wait a second... who are you?"
+    
+    if await logic.is_timed_out(client_address):
+        return "You are timed out!"
+    
+    if await logic.is_whitelisted(client_address):
+        return "You are already whitelisted!"
+    
+    # TODO make this return an HTML page.
+    return "Hello world!"
 
 
 async def login_endpoint(body: LoginBody, request: Request):
-    pass
+    nginx_address = request.client.host
+    client_address = request.headers.get("x-real-ip")
+    
+    if nginx_address != env.NGINX_HOST:
+        return "Wait a second... who are you?"
+    
+    if await logic.is_timed_out(client_address):
+        return "You are timed out!"
+        
+    if await logic.is_whitelisted(client_address):
+        return "You are already whitelisted!"
+    
+    return await logic.attempt_login(client_address, body.password)
 
 
 def launch():
